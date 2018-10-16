74d0a3aecee5524b65ce2cb3cddf14db9ae4d297
annmcalvert <annmcalvert@gmail.com>
2018-10-16 11:09:59 -0500

    key highlights done

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
 //hides uppercase when page loads
-$(document).ready(function() {
+$(document).ready(function () {
     $('#keyboard-upper-container').hide();
 });
 
 //shows uppercase when shift key is pressed
-$(document).keydown(function(e) {
+$(document).keydown(function (e) {
     if (e.which == 16) {
         $('#keyboard-upper-container').show();
         $('#keyboard-lower-container').hide();
@@ -12,23 +12,23 @@ $(document).keydown(function(e) {
 })
 
 //shows lowercase when shift key is released
-$(document).keyup(function(e) {
+$(document).keyup(function (e) {
     if (e.which == 16) {
         $('#keyboard-upper-container').hide();
         $('#keyboard-lower-container').show();
     }
 })
 
-//events when key is pressed
-$(document).keydown(function(e) {
-    let keyNumber = e.which;
-    $('#' + keyNumber).css('background-color', 'yellow');
+//gets ascii code from keypress and changes background color to yellow
+$(document).keypress(function (e) {
+    console.log('Ascii : ' + e.which);
+    let keyId = e.which;
+    $('#' + keyId).css('background-color', 'yellow');
+});
+
+//clears yellow background from all keys
+$(document).keyup(function (e) {
+    $('.key').each(function () {
+        $(this).css('background-color', '');
+    })
 })
-
-//events when key is released
-$(document).keyup(function(e) {
-    let keyNumber = e.which;
-    $('#' + keyNumber).css('background-color', '');
-})
-
-
