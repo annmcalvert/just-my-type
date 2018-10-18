3f6dfc21271e70213da0f93557105080da939dcd
annmcalvert <annmcalvert@gmail.com>
2018-10-18 10:41:43 -0500

    complete

--- scripts.js
+++ scripts.js
@@ -1,5 +1,4 @@
-//let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
-let sentences = ['first sentence', 'hello world', 'ann calvert', 'praying mantis', 'last sentence'];
+let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
 let letterIndex = 0;
 let sentenceIndex = 0;
 let mistakeNumber = 0;
